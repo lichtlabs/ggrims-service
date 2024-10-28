7fa52ec366489c9866eb7759f02dbdab32cf0285
sena <sena@lichtlabs.org>
2024-10-28 14:18:35 +0700

    style(helpers.go): format code by adding blank lines for improved readability

--- events/helpers.go
+++ events/helpers.go
@@ -12,9 +12,11 @@ func extractQuery(query *ListQuery) *ListQuery {
 	if query.Page > 0 {
 		query.Page = query.Page - 1
 	}
+
 	if query.Limit == 0 {
 		query.Limit = 10
 	}
+	
 	if query.OrderBy == "" {
 		query.OrderBy = "created_at DESC"
 	} else {
