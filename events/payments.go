30ad7725750080de6c2377c9b2af04c3f1363c4d
sena <sena@lichtlabs.org>
2024-10-29 19:26:35 +0700

    chore(payments.go): comment out redirect_url setting to prevent potential issues with bill creation

--- events/payments.go
+++ events/payments.go
@@ -70,7 +70,7 @@ func CreateBill(ctx context.Context, req *CreateBillRequest) (*CreateBillRespons
 	data.Set("amount", fmt.Sprintf("%d", req.Amount))
 	data.Set("type", req.Type)
 	data.Set("expired_date", req.ExpiredDate.Format("2006-01-02 15:04:05"))
-	data.Set("redirect_url", req.RedirectURL)
+	// data.Set("redirect_url", req.RedirectURL)
 	data.Set("is_address_required", fmt.Sprintf("%d", req.IsAddressRequired))
 	data.Set("is_phone_number_required", fmt.Sprintf("%d", req.IsPhoneNumberRequired))
 
